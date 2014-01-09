a6f0cfb2fa318b4ba6f5b6b6253ab9b29715002e
jburridge <jburridge@fullsail.edu>
2014-01-08 20:48:21 -0500

    Variables and problem solving
    Both in class work on variables and also the problem solving worksheet

---
+++ WPF/Webstorm/variables.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Justin on 1/8/14.
+ */
+
+
+//var question=prompt("What is your favorite partial number?");
+//var questionTwo=prompt("What is your favorite whole number?");
+//var thingy="Your favorite partial and whole number together is ";
+//var answer=parseFloat(question)+parseInt(questionTwo);
+//
+//
+//console.log(thingy+answer);
+//
+//console.log("I performed addition on "+question+" and "+questionTwo+" and the answer was "+answer+".");
+
+var shoppingList=["eggs","chicken","brownRice","veg"];
+console.log(shoppingList);
+console.log(shoppingList[0]);
+
+shoppingList[4]="Formula";
+shoppingList.push("Bacon");
+
+console.log(shoppingList);
+
+console.log(shoppingList.length);
