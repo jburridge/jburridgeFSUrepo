70239effb0cb9f8b12bc6d48444cddc1d2bceaa9
jburridge <jburridge@fullsail.edu>
2014-01-10 21:51:56 -0500

    Shopping Bill
    completed

--- WPF/expressions_worksheet/JavaScript/expressions_worksheet.js
+++ WPF/expressions_worksheet/JavaScript/expressions_worksheet.js
@@ -10,15 +10,24 @@
 
 //Slice of Pie part 1
 
-var pizza=prompt("How many pizzas where ordered?");
-var pizzaSlice=prompt("How many slices per pizza are their?");
-var people=prompt("How many people are attending the party?");
-var slicesOfPizza=pizzaSlice*pizza;
-var slicePerPerson=slicesOfPizza/people;
-var slicesLeftover=slicesOfPizza%people;
-//
+//var pizza=prompt("How many pizzas where ordered?");
+//var pizzaSlice=prompt("How many slices per pizza are their?");
+//var people=prompt("How many people are attending the party?");
+//var slicesOfPizza=pizzaSlice*pizza;
+//var slicePerPerson=slicesOfPizza/people;
+//var slicesLeftover=slicesOfPizza%people;
+
 //console.log("Each person at the party will get "+slicePerPerson+" slices of pizza");
 
 //Slice of Pie part 2
 
-console.log("Sparky will get "+slicesLeftover+" slices leftover.");
+//console.log("Sparky will get "+slicesLeftover+" slices leftover.");
+
+//Average Shopping Bill
+
+var grocerys=[100,150,110,130,200];
+var groceryTotal=grocerys[0]+grocerys[1]+grocerys[2]+grocerys[3]+grocerys[4];
+var groceryAverage=groceryTotal/5;
+
+console.log("The grocery total is $"+groceryTotal+" and their is a weekly average of $"+groceryAverage+" being spent.");
+
