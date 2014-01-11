be53c1c9eccda8ca6f03eb72e007613e5fe70555
jburridge <jburridge@fullsail.edu>
2014-01-11 12:56:24 -0500

    Conditionals sub
    if statement if you have enough designers and programmers

--- WPF/Conditionals subproject/Conditionals_Industry/JavaScript/industry.js
+++ WPF/Conditionals subproject/Conditionals_Industry/JavaScript/industry.js
@@ -11,3 +11,7 @@ var peopleDesignNeed=prompt("How many designers are needed for the project?");
 var peopleProgramLeft=peopleProgramNeed-peopleProgram
 var peopleDesignLeft=peopleDesignNeed-peopleDesign
 
+//checking if you have enough programmers and designers
+if (peopleProgram>=peopleProgramNeed && peopleDesign>=peopleDesignNeed) {
+//if you have enough of both then console.log
+    console.log("You do not need to hire anyone for the project");
