3b9ef2d0e28ff2ecf2276443a4d7a88a722e4ad6
jburridge <jburridge@fullsail.edu>
2014-01-11 13:23:38 -0500

    Conditionals sub
    added more variables to wacky

--- WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
+++ WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
@@ -4,3 +4,8 @@
 //setting up variables
 var oranges=parseInt(prompt("How many oranges do you have?"));
 var bananas=parseInt(prompt("How many bananas do you have?"));
+var whatToDo=prompt("Would you like to 'multiply', 'divide', 'add' or 'subtract' your oranges and bananas?");
+var orangeBan=oranges-bananas
+var banOrange=oranges+bananas
+var orangeMulBan=oranges*bananas
+var orangeDivBan=oranges/bananas
