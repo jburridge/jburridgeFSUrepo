d333bca71f7f7a5e06bfe7913a3f992ae9d54961
jburridge <jburridge@fullsail.edu>
2014-01-11 12:57:24 -0500

    Conditionals sub
    checking to see if you have enough programmers

--- WPF/Conditionals subproject/Conditionals_Industry/JavaScript/industry.js
+++ WPF/Conditionals subproject/Conditionals_Industry/JavaScript/industry.js
@@ -15,3 +15,10 @@ var peopleDesignLeft=peopleDesignNeed-peopleDesign
 if (peopleProgram>=peopleProgramNeed && peopleDesign>=peopleDesignNeed) {
 //if you have enough of both then console.log
     console.log("You do not need to hire anyone for the project");
+} else {
+//now checking to see if you have enough programmers
+    if (peopleProgram<peopleProgramNeed) {
+//if you do not have enough then console log how many programmers you still need
+        console.log("You do not have enough programmers, you need "+peopleProgramLeft+" more programmer(s).");
+
+   
