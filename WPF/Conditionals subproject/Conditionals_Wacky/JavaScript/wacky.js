992a88c92162a504cb76b05b8fa1a1c3fdd68f2f
jburridge <jburridge@fullsail.edu>
2014-01-11 13:36:47 -0500

    Conditionals sub
    subtracted yellow oranges

--- WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
+++ WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
@@ -15,4 +15,22 @@ var orangeDivBan=oranges/bananas
 if (whatToDo==multiply) {
 //console log multiplied orange bananas
     console.log("That would mean you have "+orangeMulBan+" orange bananas!");
+} else {
+//if whattodo is divide
+    if (whatToDo==divide) {
+//console log divided orange bananas
+        console.log("That would mean you have "+orangeDivBan+" orange bananas!");
+    } else {
+//if whattodo is add
+        if (whatToDo==add) {
+//console log added yellow oranges
+            console.log("What? You have "+banOrange+" yellow oranges?");
+        } else {
+//if whattodo is subtract
+            if (whatToDo==subtract) {
+//console log subtracted yellow oranges
+                console.log("Woah, You now have "+orangeBan+" yellow oranges!");
+            }
+        }
+    }
 }
