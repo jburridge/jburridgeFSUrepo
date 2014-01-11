d340b5d2f8b58936123d5d7bd3a80a1212a15aa6
jburridge <jburridge@fullsail.edu>
2014-01-11 13:06:36 -0500

    Condtionals sub
    set up variables for runtime

--- WPF/Conditionals subproject/Conditionals_Personal/JavaScript/personal.js
+++ WPF/Conditionals subproject/Conditionals_Personal/JavaScript/personal.js
@@ -1,3 +1,9 @@
 /**
  * Created by Justin on 1/11/14.
  */
+
+//setting up variables
+
+var runGoal=prompt("How long did you run yesterday?");
+var runTime=prompt("How long did you run today?");
+var runCalc=runTime-runGoal;
