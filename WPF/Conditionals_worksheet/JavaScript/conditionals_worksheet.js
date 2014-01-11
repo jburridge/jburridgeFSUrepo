ecfa36d4f5b2227126e2126435cdb41a8c4533d6
jburridge <jburridge@fullsail.edu>
2014-01-10 22:51:46 -0500

    Conditionals Celsius Fahrenheit converter
    finished converter

--- WPF/Conditionals_worksheet/JavaScript/conditionals_worksheet.js
+++ WPF/Conditionals_worksheet/JavaScript/conditionals_worksheet.js
@@ -3,11 +3,24 @@
  */
 
 //Stuff your face 1
+//
+//var compWeight=180;
+//
+//if (compWeight>=250){
+//    console.log("Qualified");
+//} else {
+//    console.log("Not Qualified");
+//}
 
-var compWeight=180;
+//Celsius Fahrenheit Converter
 
-if (compWeight>=250){
-    console.log("Qualified");
-} else {
-    console.log("Not Qualified");
-}
+//var temperature=prompt("What is your current temperature?");
+//var fOrC=prompt("F or C? (Fahrenheit or Celsius?)");
+//var fahrenheit=(temperature-32)*(5/9);
+//var celsius=temperature*(9/5)+32;
+//
+//if (fOrC.toLowerCase()=="f"){
+//    console.log(fahrenheit);
+//} else {
+//    console.log(celsius);
+//}
