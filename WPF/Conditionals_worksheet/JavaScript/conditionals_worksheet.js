60920e3b9550a785198248e624ae2909e22ca603
jburridge <jburridge@fullsail.edu>
2014-01-10 22:26:29 -0500

    Conditionals Worksheet Stuff your face 1
    Stuff your face 1 of conditionals worksheet completed

---
+++ WPF/Conditionals_worksheet/JavaScript/conditionals_worksheet.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Justin on 1/10/14.
+ */
+
+//Stuff your face 1
+
+var compWeight=180;
+
+if (compWeight>=250){
+    console.log("Qualified");
+} else {
+    console.log("Not Qualified");
+}
