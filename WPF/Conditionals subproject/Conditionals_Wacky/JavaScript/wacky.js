92e89661210bdc362f40a6c41662c408ee2e0d0d
jburridge <jburridge@fullsail.edu>
2014-01-11 13:15:53 -0500

    Conditionals sub
    setting up variables for wacky

--- WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
+++ WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
@@ -1,3 +1,6 @@
 /**
  * Created by Justin on 1/11/14.
  */
+//setting up variables
+var oranges=parseInt(prompt("How many oranges do you have?"));
+var bananas=parseInt(prompt("How many bananas do you have?"));
