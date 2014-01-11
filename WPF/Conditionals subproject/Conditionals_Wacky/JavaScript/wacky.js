93fad3b1826f49b56d4bca54c0e5d7bb614d6ff0
jburridge <jburridge@fullsail.edu>
2014-01-11 13:26:20 -0500

    Conditionals
    multiply orange bananas

--- WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
+++ WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
@@ -1,6 +1,7 @@
 /**
  * Created by Justin on 1/11/14.
  */
+
 //setting up variables
 var oranges=parseInt(prompt("How many oranges do you have?"));
 var bananas=parseInt(prompt("How many bananas do you have?"));
@@ -9,3 +10,9 @@ var orangeBan=oranges-bananas
 var banOrange=oranges+bananas
 var orangeMulBan=oranges*bananas
 var orangeDivBan=oranges/bananas
+
+//if whattodo is multiply
+if (whatToDo==multiply) {
+//console log multiplied orange bananas
+    console.log("That would mean you have "+orangeMulBan+" orange bananas!");
+}
