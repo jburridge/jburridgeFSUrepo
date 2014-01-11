dc2efe11e01709befb978bbd5b49935922555f54
jburridge <jburridge@fullsail.edu>
2014-01-11 12:55:38 -0500

    Conditionals sub
    Set up variables

---
+++ WPF/Conditionals subproject/Conditionals_Industry/JavaScript/industry.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Justin on 1/11/14.
+ */
+
+//Assigning variables
+
+var peopleProgram=prompt("How many people at work are programmers?");
+var peopleDesign=prompt("How many people at work are designers?");
+var peopleProgramNeed=prompt("How many programmers are needed for the project?");
+var peopleDesignNeed=prompt("How many designers are needed for the project?");
+var peopleProgramLeft=peopleProgramNeed-peopleProgram
+var peopleDesignLeft=peopleDesignNeed-peopleDesign
+
---
+++ WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Justin on 1/11/14.
+ */
