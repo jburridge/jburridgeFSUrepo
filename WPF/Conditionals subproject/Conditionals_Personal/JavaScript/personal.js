d60a1d2206c8b6e78986f5286360c5996f7837b2
jburridge <jburridge@fullsail.edu>
2014-01-11 13:07:26 -0500

    Conditionals sub
    if your runtime is less than 0

--- WPF/Conditionals subproject/Conditionals_Personal/JavaScript/personal.js
+++ WPF/Conditionals subproject/Conditionals_Personal/JavaScript/personal.js
@@ -7,3 +7,6 @@
 var runGoal=prompt("How long did you run yesterday?");
 var runTime=prompt("How long did you run today?");
 var runCalc=runTime-runGoal;
+
+//If your run time is less than 0
+if (runCalc<0) {
