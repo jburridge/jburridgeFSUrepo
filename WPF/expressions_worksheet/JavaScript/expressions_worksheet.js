3e9634fd0e0cb7d82e320c2e8e6c2e0270f59533
jburridge <jburridge@fullsail.edu>
2014-01-10 21:28:21 -0500

    Dog Years
    Dog years completed

---
+++ WPF/expressions_worksheet/JavaScript/expressions_worksheet.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Justin on 1/10/14.
+ */
+
+//Dog Years
+
+//var humanYears=prompt("How many human years is sparky?");
+//
+//console.log("Your dog is "+humanYears*7+" dog years old.");
+
+
+
