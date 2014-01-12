387a0c1a5e99e51e7fe20334faaee999141d0fad
jburridge <jburridge@fullsail.edu>
2014-01-11 20:41:12 -0500

    Conditionals sub
    wacky bug fixed

--- WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
+++ WPF/Conditionals subproject/Conditionals_Wacky/JavaScript/wacky.js
@@ -5,29 +5,29 @@
 //setting up variables
 var oranges=parseInt(prompt("How many oranges do you have?"));
 var bananas=parseInt(prompt("How many bananas do you have?"));
-var whatToDo=prompt("Would you like to /'multiply/', /'divide/', /'add/' or /'subtract/' your oranges and bananas?");
+var whatToDo=prompt("Would you like to 'multiply', 'divide', 'add' or 'subtract' your oranges and bananas?");
 var orangeBan=oranges-bananas;
 var banOrange=oranges+bananas;
 var orangeMulBan=oranges*bananas;
 var orangeDivBan=oranges/bananas;
 
 //if whattodo is multiply
-if (whatToDo==multiply) {
+if (whatToDo=="multiply") {
 //console log multiplied orange bananas
     console.log("That would mean you have "+orangeMulBan+" orange bananas!");
 } else {
 //if whattodo is divide
-    if (whatToDo==divide) {
+    if (whatToDo=="divide") {
 //console log divided orange bananas
         console.log("That would mean you have "+orangeDivBan+" orange bananas!");
     } else {
 //if whattodo is add
-        if (whatToDo==add) {
+        if (whatToDo=="add") {
 //console log added yellow oranges
             console.log("What? You have "+banOrange+" yellow oranges?");
         } else {
 //if whattodo is subtract
-            if (whatToDo==subtract) {
+            if (whatToDo=="subtract") {
 //console log subtracted yellow oranges
                 console.log("Woah, You now have "+orangeBan+" yellow oranges!");
             } else {
