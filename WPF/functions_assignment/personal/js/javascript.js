fc208e87f7af2a581d164c40787b3cd7173697b7
jburridge <jburridge@fullsail.edu>
2014-01-27 23:43:04 -0500

    functions assignment
    commit twelve

--- WPF/functions_assignment/personal/js/javascript.js
+++ WPF/functions_assignment/personal/js/javascript.js
@@ -3,9 +3,22 @@
  */
 
 
-console.log("Today I will teach you how to wash your hands for personal hygiene.");
+console.log("Today I will teach you how to wash your hands for personal hygiene");
+console.log("Lets find out if you've washed you're hands correctly");
 
-get hands wet
-apply soap and scrub
-rinse soap
-dry hands
+var didWet=prompt("Did you wet your hands? Yes or No");
+var didSoap=prompt("Did you apply soap and scrub? Yes or No");
+var didRinse=prompt("Did you rinse your hands? Yes or No");
+var didDry=prompt("Did you dry your hands? Yes or No");
+
+
+function calculate (didWet, didSoap, didRinse, didDry) {
+    theValue="";
+    if (didWet=="yes" && didSoap=="yes" && didRinse=="yes" && didDry=="yes") {
+        theValue=(5-7)+(2*1)-(0-2)+4;
+    } else if (didDry=="yes" || didRinse=="yes" || didSoap=="yes" || didWet=="yes") {
+        theValue=(2/1)+(7*3);
+    } else {
+        theValue=(2*3)+1;
+    }
+
