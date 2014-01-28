4d96a66db978a397602e1d641a0b7c28600dd602
jburridge <jburridge@fullsail.edu>
2014-01-28 16:15:35 -0500

    functions assignment
    nineteenth commit

--- WPF/functions_assignment/wacky/js/javascript.js
+++ WPF/functions_assignment/wacky/js/javascript.js
@@ -2,13 +2,17 @@
  * Created by Justin on 1/19/14.
  */
 
+    //setting variables that generate a random decimal number and rounds it down
 var random1=Math.floor(Math.random()*3);
 var random2=Math.floor(Math.random()*3);
 var random3=Math.floor(Math.random()*3);
 var random4=Math.floor(Math.random()*5000);
 
+//start of a function with 4 parameters
 function calculate (random1, random2, random3, random4) {
+    //setting a empty string variable
     var returnMe="";
+    //starting condition where 3 variables must be equal to fourth variable
     if (random1&&random2&&random3==random4) {
         returnMe="----------------------------That's nearly impossible, You should invest in lottery.";
     } else if (random1==random4 || random2==random4 || random3==random4) {
@@ -22,11 +26,14 @@ function calculate (random1, random2, random3, random4) {
     }
     return returnMe;
 }
+//console logging a string
 console.log("You must match at least 2 numbers to have some sort of luck...");
+//logging each random number so you can see how close you where
 console.log(random1);
 console.log(random2);
 console.log(random3);
 console.log(random4);
+//using the 4 variables set at the top, we call the function with 4 parameters
 console.log(calculate(random1, random2, random3, random4));
 
 
