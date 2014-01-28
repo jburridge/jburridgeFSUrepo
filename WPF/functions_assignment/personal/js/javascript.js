057b54b65f820bfd6e085cb47b924a263eb4878a
jburridge <jburridge@fullsail.edu>
2014-01-27 22:52:53 -0500

    functions assignment
    eleventh commit

--- WPF/functions_assignment/personal/js/javascript.js
+++ WPF/functions_assignment/personal/js/javascript.js
@@ -2,6 +2,10 @@
  * Created by Justin on 1/19/14.
  */
 
-ourDate = new Date(); // found using internet resources
 
-console.log(ourDate);
+console.log("Today I will teach you how to wash your hands for personal hygiene.");
+
+get hands wet
+apply soap and scrub
+rinse soap
+dry hands
